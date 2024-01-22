0b185e5994b93c9042da3b86b5a0afdef4c10c48
Sky Lin <linsky519@gmail.com>
2024-01-22 00:06:06 -0700

    Unable to get modal working

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,13 +1,17 @@
 import { Component, inject } from '@angular/core';
-import {MatDialog, MatDialogConfig, MatDialogModule} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { PeopleService } from '../services/people.service';
 import { PeopleResult } from '../services/interfaces';
 import { PersonDetailsComponent } from '../person-details/person-details.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-people',
   standalone: true,
-  imports: [MatDialogModule, PersonDetailsComponent],
+  imports: [
+    MatButtonModule,
+    PersonDetailsComponent,
+  ],
   templateUrl: './people.component.html',
   styleUrl: './people.component.css'
 })
@@ -15,24 +19,27 @@ export class PeopleComponent {
   private peopleService = inject(PeopleService)
   public people: PeopleResult[] = []
 
-  constructor(private dialog: MatDialog) {
+  constructor(public dialog: MatDialog) {
     this.loadPeople()
   }
 
-  openDialog() {
-    const dialogConfig = new MatDialogConfig()
-
-    dialogConfig.disableClose = true
-    dialogConfig.autoFocus = true
-
-    this.dialog.open(PersonDetailsComponent, dialogConfig)
-  }
-  
   loadPeople() {
     this.peopleService.getPeople().subscribe((people: any) => {
       console.log(people.results)
       this.people = people.results
     })
+  }
+  
+  openDialog(people: PeopleResult[]): void {
+    const dialogRef = this.dialog.open(PersonDetailsComponent, {
+      width: '1000px',
+      data: { people }
+    });
+    // console.log(people)
+    
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
 }
--- src/app/person-details/person-details.component.ts
+++ src/app/person-details/person-details.component.ts
@@ -23,6 +23,5 @@ export class PersonDetailsComponent{
     })
   }
 
-
   constructor() {}
 }
